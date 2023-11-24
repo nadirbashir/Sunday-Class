17f5b735d57c5c962db8219e68913656988d2207
nadirbashir <79132799+nadirbashir@users.noreply.github.com>
2023-11-24 14:12:08 -0800

    Update index.jsx

--- 06-08-23/src/components/Item/index.jsx
+++ 06-08-23/src/components/Item/index.jsx
@@ -6,7 +6,14 @@ export default function Item({ item, width = 300 }) {
 
   return (
     <>
-      <Card style={{ width }}>
+      <Card 
+        style={{ width }} 
+        cover={
+          <img
+            alt="example"
+            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          />
+        }>
         <h4>{item.title}</h4>
         {item.description}
         <div style={{ marginTop: "20px" }}>
